6f9fd337b105023581b48876093e1d3643451eed
Bret Wilkins <bretwilkins@gmail.com>
2024-08-01 14:51:57 -0700

    made ui clickable

--- script.js
+++ script.js
@@ -8,28 +8,16 @@ function getComputerChoice() {  //get a random int between 1 and 3
         return 2;
         } else {return 3;}
     };
-
-function getHumanChoice() {   //assign rock paper scissors to 1,2,3 and get human prompt
-    let userinput = prompt("Rock, Paper, or Scissors [R, P, or S]?");
-    if (userinput.toLowerCase() === "rock") {
-        return 1;
-    } else  if (userinput.toLowerCase() === "paper") {
-        return 2;
-    } else if  (userinput.toLowerCase() === "scissors") {
-        return 3;
-        }
-       else return null;
-    };
-
-    
-function playRound() {  //play a round   "It's a tie!"  "You lost!"   "You won!"
-    let x = getHumanChoice();
+function playRound(HumanChoice) {  //play a round   "It's a tie!"  "You lost!"   "You won!"
+    let x = HumanChoice;
     if (x === null) {
         return null;
     }
     let y = getComputerChoice();
+    returnComputer(y);
     if (x===1) {
-        console.log("You: Rock");
+        console.log("You: Rock ");
+        results.textContent +="     You: Rock ";
         if (y===1) {
             console.log("It's a tie!");
             return "tie";
@@ -42,6 +30,7 @@ function playRound() {  //play a round   "It's a tie!"  "You lost!"   "You won!"
         }
     } else if (x===2) {
         console.log("You: Paper");
+        results.textContent += "    You: Paper ";
         if (y===1) {
             console.log("You win!");
             return "win";
@@ -54,25 +43,33 @@ function playRound() {  //play a round   "It's a tie!"  "You lost!"   "You won!"
         }
       }  else {
         console.log("You: Scissors");
+        results.textContent += "    You: Scissors ";
             if (y===1) {
                 console.log("You lost!");
-                return "loss";
+                     return "loss";
             } else if (y===2) {
                 console.log("You win!");
-                return "win";
+                            return "win";
             } else {
                 console.log("Tie!")
-                return "tie";
+                        return "tie";
             }
     }
 };
+function returnComputer(y) {
+    if (y===1) {
+results.textContent = " Computer: Rock ";
+}  else if (y===2) {
+results.textContent = " Computer: Paper ";
+ } else if (y==3) {
+results.textContent = " Computer: Scissors ";
+ }
+};
+let humanScore = 0;
+let computerScore = 0;
 
-function playGame() {
-    let humanScore = 0;
-    let computerScore = 0;
-   let iterations = prompt("How many games do you want to play?");
-   for (let i=0; i < iterations; i++) {
-    let winorlose = playRound();
+function playGame(choice) {
+    let winorlose = playRound(choice);
         if (winorlose === "win") {
         humanScore++;
     } if (winorlose === "loss") {
@@ -80,12 +77,37 @@ function playGame() {
     } if (winorlose === null) {
             console.log("You didn't enter a valid value");
     } console.log("You: " + humanScore + ", Computer: " + computerScore);
-}
-   
-
+    results.textContent += `You: ${humanScore}, Computer: ${computerScore}`;
+    if (humanScore >= 5) {
+        results.textContent += " YOU WON THE FIVE ROUND GAMBIT!";
+        humanScore = 0;
+        computerScore = 0;
+    }
+    if (computerScore >= 5) {
+        results.textContent += " YOU LOSE THE FIVE ROUND GAMBIT!";
+        humanScore = 0;
+        computerScore = 0;
+    }
 };
 
-playGame();
+const results= document.querySelector('.results');
+const rpsContainer = document.querySelector('.rps-container');
+
+rpsContainer.addEventListener('click', (event) => {
+    let target = event.target;
+    switch(target.id) {
+        case 'rock':
+            playGame(1) ;
+           break;
+        case 'paper':
+            playGame(2);
+            break;
+        case 'scissors':
+            playGame(3);
+            break;
+    }
+});
+
 
 
 //console.log(getComputerChoice());
